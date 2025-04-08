c9f7bbc5213d6cde2d9dc85bd5a10f702d4a1fa0
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2025-04-08 21:36:48 +0200

    Improve JS logic

--- Newsletter-sign-up-with-success-message-main/assets/js/script.js
+++ Newsletter-sign-up-with-success-message-main/assets/js/script.js
@@ -4,38 +4,57 @@ const errorMsg = document.getElementById('error-message');
 const card = document.getElementById("card");
 const successMsg = document.getElementById('success-message');
 const dismissBtn = document.getElementById('dismiss');
+const submitBtn = document.getElementById('submit');
 
+// Empeche la soumission directe du formulaire, vérifie la validité de l'email, affiche le message de succès si oui
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    email = emailField.value.trim();
-    const isValid = validateField(email);
-    if (isValid) {
-        successMsg.classList.add('active');
+    if (validateEmailField()) {
         card.classList.remove('active');
+        successMsg.classList.add('active');
     }
-    else
-        emailField.classList.add('invalid');
 })
-emailField.addEventListener('input', () => {
-    emailField.classList.remove('invalid');
-})
+
+// vide le message d'erreur quand on saisit quelque chose
+emailField.addEventListener('input', clearError);
+// lance la vérification si le champ email perd le focus
+emailField.addEventListener('blur', validateEmailField);
+
+// cache le message de succès et reaffiche la carte quand on clique sur le bouton
 dismissBtn.addEventListener('click', () => {
     successMsg.classList.remove('active');
     card.classList.add('active');
 })
 
-function validateField(email) {
-    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+function validateEmailField() {
+    const email = emailField.value.trim();
     if (email === "") {
-        errorMsg.textContent = "Field required";
+        showError("Field required");
         return false;
     }
-    else if (!regexEmail.test(email)) {
-        errorMsg.textContent = "Valid Email required"
+    else if (!isEmailValid(email)) {
+        showError("Valid Email required");
         return false;
     }
     else {
-        errorMsg.textContent = "";
+        clearError();
         return true;
     }
+}
+function isEmailValid(email) {
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regexEmail.test(email);
+}
+
+function showError(message) {
+    errorMsg.textContent = message;
+    emailField.classList.add('invalid');
+    submitBtn.classList.add('button-invalid');
+}
+
+function clearError() {
+    errorMsg.textContent = "";
+    emailField.classList.remove('invalid');
+    submitBtn.classList.remove('button-invalid');
 }
