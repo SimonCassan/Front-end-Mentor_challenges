27c4ff89936f9ad44efdb8a904a8625434ce0782
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2025-07-03 09:12:37 +0200

    Fixes names #3

--- Blog-preview-card/README.md
+++ Blog-preview-card/README.md
@@ -21,7 +21,7 @@ Users should be able to:
 
 ### Screenshot
 
-![](./assets/images/screenshot.jpg)
+![](./assets/img/screenshot.jpg)
 
 
 ### Links
--- FAQ-accordion/script.js
+++ FAQ-accordion/script.js
@@ -2,8 +2,8 @@ const blocks = document.querySelectorAll('.h2-block');
 for (const block of blocks) {
     block.addEventListener('click', () => {
         const img = block.querySelector('img');
-        const imgSrcAttribute = img.getAttribute('src').includes('./assets/images/icon-plus.svg') ?
-            './assets/images/icon-minus.svg' : './assets/images/icon-plus.svg';
+        const imgSrcAttribute = img.getAttribute('src').includes('./assets/img/icon-plus.svg') ?
+            './assets/img/icon-minus.svg' : './assets/img/icon-plus.svg';
         img.setAttribute('src', imgSrcAttribute);
         block.closest('section').classList.toggle('expand');
     })
