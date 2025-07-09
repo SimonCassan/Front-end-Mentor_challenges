0469d7c654f75d231fb669397aaf6429115a477a
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2025-07-09 15:26:52 +0200

    React tree

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 async function fetchChallenges(): Promise<Challenge[]> {
-    const res = await fetch('/src/data/challenges.json');
+    const res = await fetch('/public/data/challenges.json');
     return res.json();
 }
 
